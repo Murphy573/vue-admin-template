40bf1139d2ca9f2ba47dc1c7f048b6783e340721
Murphy573 <429655198@qq.com>
2020-08-17 15:03:11 +0800

    feat(directive、filters)：新特性

--- src/directive/number.js
+++ src/directive/number.js
@@ -1,43 +1,74 @@
-import { isRegExp } from '@/utils/common';
+import { isRegExp, isPlainObj } from '@/utils/common';
 import { debounce } from '@/utils/debounce-throttle';
 
 /* 限制输入框只能输入number类型 */
 const Price_Pattern = /(^[1-9]([0-9]+)?(\.[0-9]{1,2})?)|(^(0){1}$)|(^[0-9]\.[0-9]([0-9])?)/;
 const Integer_Pattern = /^(0|[1-9]\d*)/;
+
+const REGEXP_MODIFIERS = ['replace', 'match'];
+// JavaScript 中最大的安全整数 2^53 - 1
+const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER;
+/**
+ * 创建处理函数
+ * @param {RegExp} regexp 正则
+ * @param {'match'|'replace'} modifier 正则使用replace or match格式化
+ * @param {Number|String} max 最大值
+ */
+const createHandler = (
+  regexp,
+  modifier = 'match',
+  max = MAX_SAFE_INTEGER,
+  min = 0
+) => {
+  if (!isRegExp(regexp)) {
+    throw new Error('请传入正则表达式！');
+  }
+  modifier = REGEXP_MODIFIERS.includes(modifier)
+    ? modifier
+    : REGEXP_MODIFIERS[0];
+  max = Number(max) || 0;
+  max = max > MAX_SAFE_INTEGER ? MAX_SAFE_INTEGER : max;
+  /**
+   * 返回处理函数
+   */
+  return value => {
+    let _res;
+    if (modifier === 'replace') {
+      _res = value.toString().replace(regexp, '');
+    }
+    else {
+      let _match = value.toString().match(regexp);
+      if (_match != null) {
+        _res = _match[0];
+      }
+      else {
+        _res = null;
+      }
+    }
+
+    if (Number(_res) > max) {
+      _res = max;
+    }
+    if (Number(_res) < min) {
+      _res = min;
+    }
+    return _res;
+  };
+};
 /**
  * 整数处理
  * @param {*} value
  */
-const IntegerHandle = value => {
-  let _match = value.toString().match(Integer_Pattern);
-  if (_match != null) return _match[0];
-  return '';
+const IntegerHandle = (max, min) => {
+  return createHandler(Integer_Pattern, 'match', max, min);
 };
 
 /**
  * 金额处理
  * @param {*} value
  */
-const PriceHandle = value => {
-  let _match = value.toString().match(Price_Pattern);
-  if (_match != null) return _match[0];
-  return '';
-};
-
-/**
- * 自定义处理
- * @param {*} value 输入的值
- * @param {RegExp} regexp 正则
- * @param {{match:true, replace:true}}} modifiers replace or match
- */
-const CustomHandle = (value, regexp, modifiers) => {
-  if (!isRegExp(regexp)) return value;
-  if (modifiers.replace) {
-    return value.toString().replace(regexp, '');
-  }
-  let _match = value.toString().match(regexp);
-  if (_match != null) return _match[0];
-  return '';
+const PriceHandle = (max, min) => {
+  return createHandler(Price_Pattern, 'match', max, min);
 };
 
 /**
@@ -54,18 +85,24 @@ const trigger = (el, type) => {
 
 /**
  * 输入框输入只能输入数字（整数和浮点型）
- * 使用方式：v-number:integer.replace="RegExp"
+ * 使用方式：v-number:integer="max"
  * :integer(默认)：可以替换成price，整数还是金额格式，忽略.replace和match
- * .replace(默认)：可以替换为match，字符串处理的方式
- * RegExp：传入的正则，当值存在时忽略integer/price，处理方式取决于.replace还是.match
+ * value：
+ *    1、输入数字的最大值,大于这个值将会重置为最大值
+ *    2、输入对象{min, max},指定最小值和最大值
  */
-const Number = {
+const NumberDirective = {
   bind (el, binding) {
-    let {
-      arg = 'integer',
-      modifiers = { replace: true },
-      value: regexp
-    } = binding;
+    let { arg = 'integer', value } = binding;
+    let max, min;
+    // 如果是对象
+    if (isPlainObj(value)) {
+      ({ max, min } = value);
+    }
+    else {
+      max = value;
+      min = 0;
+    }
     // 搜索input
     let _input = el;
     if (_input.tagName.toUpperCase() !== 'INPUT') {
@@ -74,29 +111,25 @@ const Number = {
     if (!_input) return;
     // 分情况绑定处理函数
     let handler;
-    if (regexp) {
-      handler = value => {
-        return CustomHandle(value, regexp, modifiers);
-      };
-    }
-    else if (arg === 'integer') {
-      handler = IntegerHandle;
+
+    if (arg === 'integer') {
+      handler = IntegerHandle(max, min);
     }
     else {
-      handler = PriceHandle;
+      handler = PriceHandle(max, min);
     }
     // 监听事件，去抖的方式执行处理函数
     _input.onkeyup = debounce(() => {
       _input.value = handler(_input.value);
       trigger(_input, 'input');
-    }, 500);
+    }, 1000);
     _input.onblur = debounce(() => {
       _input.value = handler(_input.value);
       trigger(_input, 'input');
-    }, 500);
+    }, 1000);
   }
 };
 
 export default function (Vue) {
-  Vue.directive('number', Number);
+  Vue.directive('number', NumberDirective);
 }
--- src/filters/global.js
+++ src/filters/global.js
@@ -3,3 +3,43 @@ import { formatDate } from '../utils/date';
 export function date (target, format) {
   return formatDate(target, format);
 }
+
+/**
+ * 对数字或者类数字字符串进行格式化
+ * @param {String<Number> | Number} target 要转换的数字或类数字字符串
+ * @param {Number} hold 保留几位小数
+ * @param {Boolean} rounding 是否四舍五入
+ */
+export function NumberFormat (target, hold = 0, rounding = true) {
+  if (Number.isNaN(Number(target))) return null;
+  let r = /^\+?[1-9][0-9]*$/;
+  hold = r.test(hold) ? Number(hold) : 0;
+  target = Number(target);
+  let _multiplier = Math.pow(10, hold);
+
+  if (rounding) {
+    return Math.round(target * _multiplier) / _multiplier;
+  }
+  else {
+    return Math.floor(target * _multiplier) / _multiplier;
+  }
+}
+
+// 金额格式化
+export function price (value, prefix = '¥') {
+  if (Number.isNaN(Number(value))) return null;
+
+  let _res = NumberFormat(value, 2);
+  let s = _res.toString().split('.');
+  // 判断数组长度确定是否补0
+  if (s.length === 1) {
+    _res = _res.toString() + '.00';
+  }
+  else if (s.length > 1) {
+    if (s[1].length < 2) {
+      _res = _res.toString() + '0';
+    }
+  }
+
+  return `${prefix}${_res}`;
+}
