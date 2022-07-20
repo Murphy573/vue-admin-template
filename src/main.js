2c69e423a24f9b7dd012e7503142874c5e659fc4
Murphy573 <429655198@qq.com>
2022-07-20 10:17:31 +0800

    feat: 先屏蔽vue错误全局处理

--- src/main.js
+++ src/main.js
@@ -24,59 +24,59 @@ Vue.use(Element, { size: 'small', zIndex: 3000 });
 Vue.config.productionTip = false;
 
 // vue所有的错误类型
-const vueRenderErrorTypes = [
-  'render',
-  // 下一次视图更新错误
-  'nextTick',
-  // 用户getter：getter for watcher \"" + (this.expression) + "\"
-  // 'getter for watcher',
-  // 用户watcher：callback for watcher \"" + (this.expression) + "\"
-  // 'callback for watcher',
-  // 立即执行的watcher：callback for immediate watcher \"" + (watcher.expression) + "\"
-  // 'callback for immediate watcher',
-  // 组件初始化获取data错误
-  // 'data()',
-  // 指令执行错误：directive ${directive.name} ${hook} hook
-  // 'directive',
-  // 异步未捕获的错误
-  // '(Promise/async)',
-  // 'created hook',
-  // 'mounted hook',
-];
-function judgeIsRenderError(info) {
-  const findError = vueRenderErrorTypes.find((errType) => {
-    return info.includes(errType);
-  });
-  return !!findError;
-}
+// const vueRenderErrorTypes = [
+//   'render',
+//   // 下一次视图更新错误
+//   'nextTick',
+//   // 用户getter：getter for watcher \"" + (this.expression) + "\"
+//   // 'getter for watcher',
+//   // 用户watcher：callback for watcher \"" + (this.expression) + "\"
+//   // 'callback for watcher',
+//   // 立即执行的watcher：callback for immediate watcher \"" + (watcher.expression) + "\"
+//   // 'callback for immediate watcher',
+//   // 组件初始化获取data错误
+//   // 'data()',
+//   // 指令执行错误：directive ${directive.name} ${hook} hook
+//   // 'directive',
+//   // 异步未捕获的错误
+//   // '(Promise/async)',
+//   // 'created hook',
+//   // 'mounted hook',
+// ];
+// function judgeIsRenderError(info) {
+//   const findError = vueRenderErrorTypes.find((errType) => {
+//     return info.includes(errType);
+//   });
+//   return !!findError;
+// }
 
-Vue.config.errorHandler = (err, vm, info) => {
-  console.log(err);
-  console.log(vm);
-  console.log(info);
-  console.log(formatComponentName(vm));
+// Vue.config.errorHandler = (err, vm, info) => {
+//   console.log(err);
+//   console.log(vm);
+//   console.log(info);
+//   console.log(formatComponentName(vm));
 
-  if (judgeIsRenderError(info)) {
-    console.log(
-      '%c==========渲染异常了==========',
-      'font-size:24px;color:blue;'
-    );
-  }
-};
+//   if (judgeIsRenderError(info)) {
+//     console.log(
+//       '%c==========渲染异常了==========',
+//       'font-size:24px;color:blue;'
+//     );
+//   }
+// };
 
-function formatComponentName(vm) {
-  if (vm.$root === vm) return 'root';
-  let name = vm._isVue
-    ? (vm.$options && vm.$options.name) ||
-      (vm.$options && vm.$options._componentTag)
-    : vm.name;
-  return (
-    (name ? 'component <' + name + '>' : 'anonymous component') +
-    (vm._isVue && vm.$options && vm.$options.__file
-      ? ' at ' + (vm.$options && vm.$options.__file)
-      : '')
-  );
-}
+// function formatComponentName(vm) {
+//   if (vm.$root === vm) return 'root';
+//   let name = vm._isVue
+//     ? (vm.$options && vm.$options.name) ||
+//       (vm.$options && vm.$options._componentTag)
+//     : vm.name;
+//   return (
+//     (name ? 'component <' + name + '>' : 'anonymous component') +
+//     (vm._isVue && vm.$options && vm.$options.__file
+//       ? ' at ' + (vm.$options && vm.$options.__file)
+//       : '')
+//   );
+// }
 
 new Vue({
   el: '#app',
