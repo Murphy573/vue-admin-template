8ac5bbce838aab284d9711e0d000ee8fbc4187ff
Murphy573 <429655198@qq.com>
2022-08-09 16:36:07 +0800

    feat: 项目工程配置

--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -2,17 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 module.exports = {
   entry: {
-    vendor: ['vue', 'vue-i18n', 'vue-router', 'vuex', 'axios']
+    vendor: ['vue', 'vue-i18n', 'vue-router', 'vuex', 'axios'],
   },
   output: {
     path: path.resolve(__dirname, '../library/'),
     filename: '[name].dll.js',
-    library: '[name]_library'
+    library: '[name]_library',
   },
   plugins: [
     new webpack.DllPlugin({
       path: path.join(__dirname, '../library/', '[name]-manifest.json'),
-      name: '[name]_library'
-    })
-  ]
+      name: '[name]_library',
+    }),
+  ],
 };
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,26 +1,26 @@
 const path = require('path');
-const OS = require('os');
+// const OS = require('os');
 // 环境变量
 const ENV = process.env;
 // 多进程压缩
-const HappyPack = require('happypack');
+// const HappyPack = require('happypack');
 // gzip压缩插件
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
-const happyThreadPool = HappyPack.ThreadPool({ size: OS.cpus().length });
+// const happyThreadPool = HappyPack.ThreadPool({ size: OS.cpus().length });
 const resolve = (dir) => path.join(__dirname, dir);
 
 // webpack plugins
 let _plugins = [
-  new HappyPack({
-    id: 'happybabel',
-    loaders: [
-      {
-        loader: 'babel-loader?cacheDirectory=true',
-      },
-    ],
-    threadPool: happyThreadPool,
-    verbose: true,
-  }),
+  // new HappyPack({
+  //   id: 'happybabel',
+  //   loaders: [
+  //     {
+  //       loader: 'babel-loader?cacheDirectory=true',
+  //     },
+  //   ],
+  //   threadPool: happyThreadPool,
+  //   verbose: true,
+  // }),
 ];
 
 // 如果执行build命令，添加gzip压缩
@@ -109,12 +109,12 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        include: resolve('src'),
-        exclude: /node_modules/,
-        loader: 'happypack/loader?id=happybabel',
-      },
+      // {
+      //   test: /\.js$/,
+      //   include: resolve('src'),
+      //   exclude: /node_modules/,
+      //   loader: 'happypack/loader?id=happybabel',
+      // },
     ],
   },
   plugins: _plugins,
